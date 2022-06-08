b7a832379b845fafe1e5cb1b540336b07fa6a71d
AxolotlHelmet <dkamler@ncstrojans.com>
2022-06-08 13:08:58 +0000

    Temp State

--- src/main/scala/functions/Fibonacci.scala
+++ src/main/scala/functions/Fibonacci.scala
@@ -5,8 +5,10 @@ object Fibonacci {
   // TASK
   //    Implement fibonacci.   Given n, compute the nth Fibonacci number.
   def fibonacci(n: Int): Int = {
+    if (n == 0)
+      1
+      else if ()
     
-
   }
 
   def main(argv: Array[String]): Unit = {
